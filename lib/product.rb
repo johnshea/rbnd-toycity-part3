b18776ddd3605ce7a63cf37158b10dd86a33b09a
John Shea <sheajohnh@gmail.com>
2016-01-15 20:28:41 -0500

    Add methods and update initialize
    - Add method in_stock?
    - Add class method find_by_title
    - Add additional fields to method initialize

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,13 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+
     if @@products.none? { |el| el.title == @title }
       add_to_products
     else
@@ -12,8 +15,16 @@ class Product
     end
   end
 
+  def in_stock?
+    @stock > 0
+  end
+
   def self.all
     @@products
+  end
+
+  def self.find_by_title(title_to_find)
+    @@products.find { |el| el.title == title_to_find }
   end
 
   private
