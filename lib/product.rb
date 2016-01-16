a1a4cfce83149c5e939f728355c634de8e1bde71
John Shea <sheajohnh@gmail.com>
2016-01-15 20:03:44 -0500

    Add check for duplicates error

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,25 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    if @@products.none? { |el| el.title == @title }
+      add_to_products
+    else
+      raise DuplicateProductError, "#{self.title} already exists."
+    end
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    @@products << self
+    #puts self.inspect
   end
 end
